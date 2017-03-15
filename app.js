a81337d3afc7812a5b1e5eb7835df85173794f89
Davis, Jermaine <jermaine_a_davis@homedepot.com>
2017-03-15 06:45:45 -0400

    Adding jade... and renaming files

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
